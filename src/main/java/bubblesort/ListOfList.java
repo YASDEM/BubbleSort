6adb9fc7fe5fcacfe1c9be2fc6e3bbd7745e290c
Römelt <alexander.roemelt@accenture.com>
2020-02-10 16:10:29 +0100

    fixed slice method

--- src/main/java/bubblesort/ListOfList.java
+++ src/main/java/bubblesort/ListOfList.java
@@ -7,13 +7,13 @@ public class ListOfList {
 	public static List<int[]> sliceList(int[] numbers, int size, boolean discard){
 		List<int[]> result = new ArrayList<int[]>();
 		
-		int numSlices = numbers.length / size;
+		int numSlices = numbers.length / size + 1;
 		
 		outer: for (int i = 0; i < numSlices; i++) {
 			int[] slice = new int[size];
 			for (int j = 0; j < size; j++) {
 				try {
-					slice[j] = numbers[i+j];
+					slice[j] = numbers[size*i+j];
 				} catch (ArrayIndexOutOfBoundsException e) {
 					if (discard) {
 						result.add(slice);
