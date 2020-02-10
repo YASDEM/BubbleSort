9c57097e0d7f07910d7d4f97efa4c7667caefe09
Römelt <alexander.roemelt@accenture.com>
2020-02-10 15:49:15 +0100

    added slice list feature

---
+++ src/main/java/bubblesort/ListOfList.java
@@ -0,0 +1,29 @@
+package bubblesort;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListOfList {
+	public static List<int[]> sliceList(int[] numbers, int size, boolean discard){
+		List<int[]> result = new ArrayList<int[]>();
+		
+		int numSlices = numbers.length / size;
+		
+		outer: for (int i = 0; i < numSlices; i++) {
+			int[] slice = new int[size];
+			for (int j = 0; j < size; j++) {
+				try {
+					slice[j] = numbers[i+j];
+				} catch (ArrayIndexOutOfBoundsException e) {
+					if (discard) {
+						result.add(slice);
+					}
+					break outer;
+				}
+			}
+			result.add(slice);
+		}
+		
+		return result;
+	}
+}
